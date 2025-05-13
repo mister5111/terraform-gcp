a05171d1ecea9479f08d46347b82384d630ba03f
kodi <kodi@gmail.com>
2025-05-13 09:42:45 +0300

    modified:   GCP/modules/europe/instance.tf

--- GCP/modules/europe/instance.tf
+++ GCP/modules/europe/instance.tf
@@ -1,6 +1,6 @@
-resource "google_compute_instance" "docker-builder" {
-  count         =  1
-  name          = format("%s-%s-instance-%02d-docker-builder", var.company, var.env, count.index + 1)
+resource "google_compute_instance" "docker" {
+  count         =  0
+  name          = format("%s-%02d-docker", var.env, count.index + 1)
   machine_type  = var.type_machine[2]
   zone          = format("%s-a",var.zone_names[1])
   tags          = ["ssh","http","https","icmp"]
@@ -15,12 +15,7 @@ resource "google_compute_instance" "docker-builder" {
   metadata = {
     startup-script = <<SCRIPT
     apt-get -y update
-    apt-get -y upgrade
-    addgroup admin
-    echo "# User rules for admin" >> /etc/sudoers.d/admin-group-sudoers
-    echo "%admin ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/admin-group-sudoers
-    useradd -g admin -s /bin/bash -m satrier && sudo -u satrier -s sh -c 'mkdir -p ~/.ssh && chmod 700 ~/.ssh && touch ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys && echo "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAl7kTY6vl33ACKbG2xO4WTTnriyHW28saAPe/1AZLuTYlH8diIlohplRydPRQXxM0JqY2h/SEPCe5JU1Rphh9tFZneg4BPf2eoLzOEnBaTazlWjhWlxfZoIA7x7JJvFXTxy7Z9kWwCLQEgPxs//Lxor3y0/7WK9EOtg8qNo5cCZ6KmClzbJfzeqoZS0GFC9asEBavWZ3YBbVi76o7TZ3kTlHyAalfRYCrzdVBEh0+C4iwcXQ9iESBGE2Hc+kSWf8axI4qK4lfdpqeOxcGsThX7YAKe0AwrSCjnA5atBuVtEIKoUS8bzWnXRGgM2XXmNxF6G8BOQZB/7vfJF+7W+qI0w== satrier" >> ~/.ssh/authorized_keys'
-    
+    apt-get -y upgrade    
     SCRIPT
     } 
 
