3ac922bad4e03e5303326fbe399ff3e47693bd4d
kodi <kodi@gmail.com>
2025-05-15 15:50:25 +0300

    terraform fmt

--- GCP/main/main.tf
+++ GCP/main/main.tf
@@ -4,44 +4,44 @@ provider "google" {
 
 terraform {
   backend "gcs" {
-    bucket  = "kodi-terraform-state-bucket"
-    prefix  = "terraform/state"
+    bucket = "kodi-terraform-state-bucket"
+    prefix = "terraform/state"
   }
 }
 
 module "global" {
-  source            = "../modules/global" 
-  env               = "${terraform.workspace}"
-  external_cidr     = var.public_subnet
-  internal_cidr     = var.private_subnet
+  source        = "../modules/global"
+  env           = terraform.workspace
+  external_cidr = var.public_subnet
+  internal_cidr = var.private_subnet
 }
 
 module "europe" {
-  source                = "../modules/europe"
-  internal_cidr         = var.private_subnet
-  network_global        = module.global.global_firewall_name
-  env                   = "${terraform.workspace}"
-  zone_names            = var.zone_list_europe
-  name_and_zone         = local.create_instances_for_each
-  type_machine          = local.selected_machine_type
-  counts                = local.create_instances_count
+  source         = "../modules/europe"
+  internal_cidr  = var.private_subnet
+  network_global = module.global.global_firewall_name
+  env            = terraform.workspace
+  zone_names     = var.zone_list_europe
+  name_and_zone  = local.create_instances_for_each
+  type_machine   = local.selected_machine_type
+  counts         = local.create_instances_count
 }
 
 module "k8s_api_service" {
-  source                = "../modules/k8s/api-service"
-  internal_cidr         = var.private_subnet_k8s
-  network_global        = module.global.global_firewall_name
-  env                   = "${terraform.workspace}"
-  zone_names            = var.zone_list_europe
-  type_machine          = var.machine_type_list_europe
-  project               = var.project
-  counts                = local.create_k8s_count
+  source         = "../modules/k8s/api-service"
+  internal_cidr  = var.private_subnet_k8s
+  network_global = module.global.global_firewall_name
+  env            = terraform.workspace
+  zone_names     = var.zone_list_europe
+  type_machine   = var.machine_type_list_europe
+  project        = var.project
+  counts         = local.create_k8s_count
 }
 
 module "postgreSQL" {
-  source                = "../modules/cloud-sql/postgreSQL"
-  env                   = "${terraform.workspace}"
-  zone_names            = var.zone_list_europe
+  source     = "../modules/cloud-sql/postgreSQL"
+  env        = terraform.workspace
+  zone_names = var.zone_list_europe
 }
 
 output "global" {
--- GCP/main/variable.tf
+++ GCP/main/variable.tf
@@ -1,5 +1,5 @@
 variable "project" {
-        default = "k8s-kodi-cluster"
+  default = "k8s-kodi-cluster"
 }
 
 locals {
@@ -15,70 +15,70 @@ locals {
 locals {
   # VM create in dev and prod
   create_instances_for_each = terraform.workspace == "dev" ? {
-#     ghost1 = "europe-west4-a"
-#     ghost2 = "europe-west4-a"
-#     ghost3 = "europe-west4-a"
-#     ghost4 = "europe-west4-a"
-  } : terraform.workspace == "prod" ? {
-#     ghost1 = "europe-west4-a"
-#     ghost2 = "europe-west4-a"
-#     ghost3 = "europe-west4-a"
-#     ghost4 = "europe-west4-a"
+    #     ghost1 = "europe-west4-a"
+    #     ghost2 = "europe-west4-a"
+    #     ghost3 = "europe-west4-a"
+    #     ghost4 = "europe-west4-a"
+    } : terraform.workspace == "prod" ? {
+    #     ghost1 = "europe-west4-a"
+    #     ghost2 = "europe-west4-a"
+    #     ghost3 = "europe-west4-a"
+    #     ghost4 = "europe-west4-a"
   } : {}
 }
 
 variable "public_subnet" {
-        default = "0.0.0.0/0"
+  default = "0.0.0.0/0"
 }
 
 variable "private_subnet" {
-        type = list(string)
-        default = [
-                "172.16.1.0/24",
-                "172.16.2.0/24",
-                "172.16.3.0/24",
-                "172.16.4.0/24"
-        ]
+  type = list(string)
+  default = [
+    "172.16.1.0/24",
+    "172.16.2.0/24",
+    "172.16.3.0/24",
+    "172.16.4.0/24"
+  ]
 }
 
 variable "private_subnet_k8s" {
-        type = list(string)
-        default = ["10.0.0.0/20"]
+  type    = list(string)
+  default = ["10.0.0.0/20"]
 }
 
 variable "zone_list_europe" {
-        type = list(string)
-        default = [
-                "europe-west1",
-                "europe-west2",
-                "europe-west3",
-                "europe-west4",
-                "europe-west5",
-                "europe-west6",
-                "europe-west8",
-                "europe-west9",
-                "europe-west10",
-                "europe-west12",
-                "europe-central2",
-                "europe-north1",
-                "europe-southwest1",
-        ]
+  type = list(string)
+  default = [
+    "europe-west1",
+    "europe-west2",
+    "europe-west3",
+    "europe-west4",
+    "europe-west5",
+    "europe-west6",
+    "europe-west8",
+    "europe-west9",
+    "europe-west10",
+    "europe-west12",
+    "europe-central2",
+    "europe-north1",
+    "europe-southwest1",
+  ]
 }
 
 variable "machine_type_list_europe" {
-        type = list(string)
-        default = [
-                "e2-micro",
-                "e2-small",
-                "e2-medium"
-        ]
+  type = list(string)
+  default = [
+    "e2-micro",
+    "e2-small",
+    "e2-medium"
+  ]
 }
 
 
 locals {
   machine_type_map = {
-    dev     = var.machine_type_list_europe[0]
-    prod    = var.machine_type_list_europe[2]
+    dev  = var.machine_type_list_europe[0]
+    prod = var.machine_type_list_europe[2]
   }
 
   selected_machine_type = lookup(local.machine_type_map, terraform.workspace, var.machine_type_list_europe[1])
